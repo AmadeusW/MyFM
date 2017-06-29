0ad075cc57be01997972e29e3a2cb70f54692e74
Amadeusz Wieczorek <git@amadeusw.com>
2017-06-29 15:53:43 -0700

    Creates solution; Adds core project

---
+++ src/MyFm.Core/Location.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyFm.Core
+{
+    public class Location
+    {
+        public string Path { get; }
+        public bool IsSource { get; }
+        public bool IsDestination { get; }
+
+        public Location(string path, bool isSource, bool isDestination)
+        {
+            Path = path;
+            IsSource = isSource;
+            IsDestination = isDestination;
+        }
+
+        public Location WithPath(string path) =>
+            new Location(path, IsSource, IsDestination);
+        public Location AsSource(bool isSource) =>
+            new Location(Path, isSource, IsDestination);
+        public Location AsDestination(bool isDestination) =>
+            new Location(Path, IsSource, isDestination);
+    }
+}
---
+++ src/MyFm.Core/State.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MyFm.Core
+{
+    class State
+    {
+        List<Location> Locations { get; } = new List<Location>();
+
+        public State()
+        {
+
+        }
+
+        public void AddLocation(Location location)
+        {
+            Locations.Add(location);
+        }
+
+        public IEnumerable<string> GetContents(Location location)
+        {
+            return Directory.EnumerateFileSystemEntries(location.Path);
+        }
+
+
+    }
+}
